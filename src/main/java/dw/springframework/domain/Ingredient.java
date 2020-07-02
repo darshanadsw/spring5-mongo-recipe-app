b27824f108f77cc3428e0c54542c4c3a020a7df4
darshanadsw <darshanadsw@gmail.com>
2020-07-02 10:55:57 -0400

    Code refactoring

--- src/main/java/dw/springframework/domain/Ingredient.java
+++ src/main/java/dw/springframework/domain/Ingredient.java
@@ -2,20 +2,25 @@ package dw.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 
 @Getter
 @Setter
+@Document
 public class Ingredient {
 
+    @Id
     private String id;
     private String description;
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
-    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -30,7 +35,6 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        this.recipe = recipe;
     }
 
 }
--- src/main/java/dw/springframework/domain/Notes.java
+++ src/main/java/dw/springframework/domain/Notes.java
@@ -2,13 +2,16 @@ package dw.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Getter
 @Setter
+@Document
 public class Notes {
 
+    @Id
     private String id;
     private Recipe recipe;
     private String recipeNotes;
